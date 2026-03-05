043fee59a3256efc9e1d3203eca4b6fd1dfd2d6b
LRVT <21357789+l4rm4nd@users.noreply.github.com>
2026-03-05 16:12:04 +0100

    feat: bump dependencies

--- apps/fz_http/priv/repo/migrations/20221129002233_update_on_delete_behavior.exs
+++ apps/fz_http/priv/repo/migrations/20221129002233_update_on_delete_behavior.exs
@@ -2,32 +2,10 @@ defmodule FzHttp.Repo.Migrations.UpdateOnDeleteBehavior do
   use Ecto.Migration
 
   def change do
-    drop(constraint(:oidc_connections, "oidc_connections_user_id_fkey"))
+    execute("ALTER TABLE oidc_connections DROP CONSTRAINT IF EXISTS oidc_connections_user_id_fkey")
+    execute("ALTER TABLE oidc_connections ADD CONSTRAINT oidc_connections_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE")
 
-    alter table(:oidc_connections) do
-      modify(
-        :user_id,
-        references(:users, on_delete: :delete_all),
-        null: false,
-        from: {
-          references(:users, on_delete: :nothing),
-          null: false
-        }
-      )
-    end
-
-    drop(constraint(:mfa_methods, "mfa_methods_user_id_fkey"))
-
-    alter table(:mfa_methods) do
-      modify(
-        :user_id,
-        references(:users, on_delete: :delete_all),
-        null: false,
-        from: {
-          references(:users, on_delete: :nothing),
-          null: false
-        }
-      )
-    end
+    execute("ALTER TABLE mfa_methods DROP CONSTRAINT IF EXISTS mfa_methods_user_id_fkey")
+    execute("ALTER TABLE mfa_methods ADD CONSTRAINT mfa_methods_user_id_fkey FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE")
   end
 end
